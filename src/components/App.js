6a8a89ed6f32ad7fc90c871486aeb73f4e3a1868
thejuprasad <73605401+thejuprasad@users.noreply.github.com>
2023-05-28 16:12:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -50,5 +50,97 @@ onClick={() => handleKeyPress(key)}
 </div>
 );
 };
+import "../styles/App.css";
 
+import React, { useState, useEffect } from "react";
+
+const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+
+const App = () => {
+
+  const [previewText, setPreviewText] = useState("");
+
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+
+    if (previewText.toLowerCase() === "forty two") {
+
+      fetch("https://api.quotable.io/random")
+
+        .then((response) => response.json())
+
+        .then((data) => setQuote(data.content));
+
+    } else {
+
+      setQuote("");
+
+    }
+
+  }, [previewText]);
+
+  const handleKeyPress = (e) => {
+
+    const key = e.target.innerHTML;
+
+    if (key === "Space") {
+
+      setPreviewText((prevText) => prevText + " ");
+
+    } else {
+
+      setPreviewText((prevText) => prevText + key.toLowerCase());
+
+    }
+
+  };
+
+  return (
+
+    <>
+
+      {previewText.toLowerCase() === "forty two" ? (
+
+        <div className="quote">{quote}</div>
+
+      ) : (
+
+        <div className="keyboard">
+
+          <div className="preview">{previewText}</div>
+
+          <div>
+
+            {keys.map((key) => (
+
+              <button
+
+                key={key}
+
+                id={key === " " ? `key-space` : `key-${key}`}
+
+                onClick={handleKeyPress}
+
+              >
+
+                {key === " " ? "Space" : key.toUpperCase()}
+
+              </button>
+
+            ))}
+
+          </div>
+
+        </div>
+
+      )}
+
+    </>
+
+  );
+
+};
+
+export default App;
 export default App;
